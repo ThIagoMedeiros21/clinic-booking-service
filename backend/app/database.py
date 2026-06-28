efc30b9d4dce4efa4f3ecda20472b1f15bf44733
ThIagoMedeiros21 <thfmadeiros01@gmail.com>
2026-06-28 18:09:21 -0300

    Creation all configs backend

---
+++ backend/app/database.py
@@ -0,0 +1,34 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+from fastapi import HTTPException
+from sqlalchemy.exc import OperationalError
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError(
+        "Missing required environment variable 'DATABASE_URL'. "
+        "Set it before starting the application."
+    )
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind = engine, autoflush=False, autocommit = False)
+
+Base = declarative_base
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    except OperationalError:
+        raise HTTPException(
+            status_code=503,
+            detail="Não foi possível conectar ao banco de dados."
+        )
+
+    finally:
+        db.close()
